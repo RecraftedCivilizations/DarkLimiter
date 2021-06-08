30f6c83e7f080a8715cee28689c6f01294f245a5
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-06-08 13:46:58 +0200

    feat: Register the block limiter and the clearAllBlocks command

--- src/main/kotlin/com/github/recraftedcivilizations/darklimiter/DarkLimiter.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/darklimiter/DarkLimiter.kt
@@ -2,6 +2,7 @@ package com.github.recraftedcivilizations.darklimiter
 
 import com.github.darkvanityoflight.recraftedcore.ARecraftedPlugin
 import com.github.recraftedcivilizations.darklimiter.commands.*
+import com.github.recraftedcivilizations.darklimiter.limiters.BlockLimiter
 import com.github.recraftedcivilizations.darklimiter.limiters.DoorLocker
 import com.github.recraftedcivilizations.darklimiter.parser.ConfigParser
 import com.github.recraftedcivilizations.darklimiter.parser.dataparser.YMLDataSource
@@ -20,6 +21,10 @@ class DarkLimiter: ARecraftedPlugin() {
         Bukkit.getPluginManager().registerEvents(locker, this)
         Bukkit.getPluginManager().registerEvents(selector, this)
 
+        // Register the block limiter
+        val blockLimiter = BlockLimiter(configParser)
+        Bukkit.getPluginManager().registerEvents(blockLimiter, this)
+
         // Register all commands
         getCommand("lock")?.setExecutor(LockDoorForPlayer(locker, selector))
         getCommand("unlock")?.setExecutor(UnlockPlayerDoor(locker, selector))
@@ -27,6 +32,7 @@ class DarkLimiter: ARecraftedPlugin() {
         getCommand("unlockGroupDoor")?.setExecutor(UnlockGroupDoors(locker, selector))
         getCommand("reset")?.setExecutor(ResetAllDoors(locker))
         getCommand("forceUnlock")?.setExecutor(ForceUnlock(locker, selector))
+        getCommand("clearAllBlocks")?.setExecutor(ClearAllBlocks(blockLimiter))
     }
 
 }
